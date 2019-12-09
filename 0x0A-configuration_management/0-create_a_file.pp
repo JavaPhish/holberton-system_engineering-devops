ec4bd759c69edab7497767f868cd776158a26eee
JavaPhish <cclements3150@gmail.com>
2019-12-09 23:16:12 +0000

    [Fix] Change perms and lint

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
-file { '/tmp/holberton':
-  path   => '/tmp/holberton',
-  ensure   => 'present',
-  content   => '"I love Puppet"',
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode   => '0744'
+file { '/tmp/holberton' :
+  ensure   =>  'present',
+  path     =>  '/tmp/holberton',
+  content  =>  'I love Puppet',
+  owner    =>  'www-data',
+  group    =>  'www-data',
+  mode     =>  '0744'
 }
