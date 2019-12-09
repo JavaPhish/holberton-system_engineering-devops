04f0b83cf7c31369c384c1e9a08cb094f026dfea
JavaPhish <cclements3150@gmail.com>
2019-12-09 23:52:14 +0000

    Fix

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,6 @@
 # Kills the command killmenow
 exec { 'Kills killmenow' :
-  command  => 'pkill killmenow'
+  command  => 'pkill killmenow',
+  path   => '/usr/bin:/usr/sbin:/bin',
   creates  => '/killmenow'
 }
