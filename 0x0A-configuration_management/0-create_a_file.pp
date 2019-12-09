181d42714690d2c5e38ec526299007709ceee562
JavaPhish <cclements3150@gmail.com>
2019-12-09 23:35:23 +0000

    Um what.

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,5 +1,5 @@
 file { 'holberton' :
-  ensure   =>  'present',
+  ensure   =>  '{md5}f1b70c2a42a98d82224986a612400db9',
   path     =>  '/tmp/holberton',
   content  =>  'I love Puppet',
   owner    =>  'www-data',
