0ba5c0e2b7493273350d74491e8cd259cc9af469
JavaPhish <31843656+JavaPhish@users.noreply.github.com>
2019-12-10 19:40:05 -0500

    Update 4-puppet_ssh_config.pp

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,10 +1,10 @@
 
-# Creates a file n stuff
+# Removes password authentication
 file_line { 'Turn off passwd auth' :
   path     =>  '/etc/ssh/ssh_config',
   line     =>  'PasswordAuthentication no'
 }
-
+# Enforces use of the holberton key in the ssh folder
 file_line { 'Declare identity file' :
   path    => '/etc/ssh/ssh_config',
   line    => 'IdentityFile ~/.ssh/holberton',
