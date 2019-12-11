bab41790e9995d37f896d504d6166bfae967ae06
JavaPhish <31843656+JavaPhish@users.noreply.github.com>
2019-12-10 19:38:36 -0500

    Update 4-puppet_ssh_config.pp

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,7 +1,7 @@
 
 # Creates a file n stuff
 file_line { 'Turn off passwd auth' :
-  path     =>  '~/ssh/ssh_config',
+  path     =>  '/etc/ssh/ssh_config',
   line     =>  'PasswordAuthentication no'
 }
 
