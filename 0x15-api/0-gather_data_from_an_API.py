57ebac8a8d69d0c4e49e12607289bcca66de3b42
JavaPhish <cclements3150@gmail.com>
2020-09-14 06:04:43 -0400

    Makes an API request to https://jsonplaceholder.typicode.com/

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+""" Test with jsonplaceholder API """
+
+import requests
+import json
+import sys
+
+
+if __name__ == "__main__":
+
+    """ Get the todos matching the UserId provided by argv """
+    api_url = "https://jsonplaceholder.typicode.com"
+    api = requests.get('{}/todos?userId={}'.format(api_url, sys.argv[1]))
+    todo_d = json.loads(api.content)
+
+    task_total = 0
+    task_completed = 0
+
+    for task in todo_d:
+        """ Count all the tasks and tasks completed (list of tasks) """
+        task_total += 1
+
+        if task.get('completed') is True:
+            task_completed += 1
+
+    """ Need to get the matching users name with the ID from the post """
+    api2 = requests.get('{}/users?id={}'.format(api_url, sys.argv[1]))
+    name = json.loads(api2.content)
+    """ All content is his ID so the first dict will work """
+    name = name[0].get('name')
+
+    """ First print string :) """
+    print('Employee {} is done with tasks({}/{}):'.format(name,
+                                                          task_completed,
+                                                          task_total))
+
+    """ print all the tasks (Just gonna re use todo_data) """
+    for task in todo_d:
+        print('\t {}'.format(task.get('title')))
