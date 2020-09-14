3e2c6e8bdc1a89eb75a8768d289fd0a342de3f32
JavaPhish <cclements3150@gmail.com>
2020-09-14 18:06:53 -0400

    pep8 fix

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     """ All content is his ID so the first dict will work """
     name = name[0].get('username')
 
-    """ Now we have the name, and the todos list data, lets do csv stuff with it"""
+    """ Now we have the name, and the todos list data, lets do csv stuff"""
     """ Using our data, write each line of a CSV file to contain a task """
     with open('{}.csv'.format(argv[1]), mode='w') as file:
         for task in todo_d:
